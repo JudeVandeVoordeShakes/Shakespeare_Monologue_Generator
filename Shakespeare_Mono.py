fb289b68e9661d33b6492d8eebb45db2c76186e5
JudeVandeVoordeShakes <87096465+JudeVandeVoordeShakes@users.noreply.github.com>
2021-07-16 12:36:55 -0400

    Create Shakespeare_Mono.py

---
+++ Shakespeare_Mono.py
@@ -0,0 +1,86 @@
+class three_two:
+  def __init__(self, alpha, omega, speaker, play)
+  self.play = play
+  self.speaker = speaker
+  self.alpha = alpha
+  self.omega = omega
+  pass
+import random from random
+def get_three_two(number):  
+  three_two_instance = random.randint(1, 38)
+  if three_two_instance = 1:
+    alls_well_that_ends_well = three_two()
+  elif three_two_instance = 2:
+    antony_and_cleopatra = three_two()
+  elif three_two_instance = 3:
+    
+  elif three_two_instance = 4:
+    
+  elif three_two_instance = 5:
+    
+  elif three_two_instance = 6:
+    
+  elif three_two_instance = 7:
+    
+  elif three_two_instance = 8:
+    
+  elif three_two_instance = 9:
+    
+  elif three_two_instance = 10:
+    
+  elif three_two_instance = 11:
+    
+  elif three_two_instance = 12:
+    
+  elif three_two_instance = 13:
+    
+  elif three_two_instance = 14:
+    
+  elif three_two_instance = 15:
+    
+  elif three_two_instance = 16:
+    
+  elif three_two_instance = 17:
+    
+  elif three_two_instance = 18:
+    
+  elif three_two_instance = 19:
+    
+  elif three_two_instance = 20:
+    
+  elif three_two_instance = 21:
+    
+  elif three_two_instance = 22:
+    
+  elif three_two_instance = 23:
+    
+  elif three_two_instance = 24:
+    
+  elif three_two_instance = 25:
+    
+  elif three_two_instance = 26:
+    
+  elif three_two_instance = 27:
+    
+  elif three_two_instance = 28:
+    
+  elif three_two_instance = 29:
+    
+  elif three_two_instance = 30:
+    
+  elif three_two_instance = 31:
+    
+  elif three_two_instance = 32:
+    
+  elif three_two_instance = 33:
+    
+  elif three_two_instance = 34:
+    
+  elif three_two_instance = 35:
+    
+  elif three_two_instance = 36:
+    
+  else:
+    
+
+  
